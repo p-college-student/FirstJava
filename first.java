afde60fe89eae7e538fe2b552e43f8fdafe8d943
Aman Maner <145569001+p-college-student@users.noreply.github.com>
2024-07-03 21:54:05 +0530

    addText

--- first.java
+++ first.java
@@ -3,6 +3,9 @@ class first
        {   public static void main(String args[])
        {
                System.out.println("HelloWorld !!!");
+
+               System.out.println("Thise is our world ") ;
+
        }
 
 }
