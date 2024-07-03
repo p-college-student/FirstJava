8363d954c406e31b7bd18f54ce382d581f0c388c
Aman Maner <145569001+p-college-student@users.noreply.github.com>
2024-07-03 16:46:15 +0530

    space managed

--- first.java
+++ first.java
@@ -1,13 +1,8 @@
 class first
 {
-
-       public static void main(String args[])
+       {   public static void main(String args[])
        {
-
                System.out.println("Hello World !!!");
-                    
-
        }
-
 
 }
