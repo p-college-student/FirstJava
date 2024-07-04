7df20f02789c35d328102f0f6687c48259a1d80a
Aman Maner <145569001+p-college-student@users.noreply.github.com>
2024-07-04 16:30:38 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,36 @@
+class Arithmetic
+{
+
+          public static void main(String args[])
+          {
+
+             int a,b,c;
+
+                 a=200;
+                 b=100;
+               
+                   c=a+b;
+
+                 System.out.println("Addition ="+c);
+
+                 c=a-b;
+
+                 System.out.println("Substraction ="+c);
+
+                 c=a*b;
+
+                 System.out.println("Multiplication ="+c);
+
+                 c=a%b;
+
+                 System.out.println("Remainder ="+c);
+
+                 c=a/b;
+
+                 System.out.println("Division ="+c);
+
+
+
+          }
+
+}
